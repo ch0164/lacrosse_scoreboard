bebfc6dfb1e5a97e2c824091de41010f0dfb05d8
Christian Hall <ch0164@uah.edu>
2022-01-16 02:11:29 -0600

    Removed obsolete references

--- scoreboard/settings.py
+++ scoreboard/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
-    'playground',
     'debug_toolbar',
     'rest_framework',
 ]
