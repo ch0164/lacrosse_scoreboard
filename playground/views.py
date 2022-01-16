b8e5fe9c3b88460be77cf9e20187b1bacf7a07cf
Christian Hall <ch0164@uah.edu>
2022-01-16 02:09:57 -0600

    Deleted playground used for testing

--- playground/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
-
-# Create your views here.
-def main(request: HttpRequest) -> HttpResponse:
-    return render(request, "hello.html", {"name": "Christian"})
