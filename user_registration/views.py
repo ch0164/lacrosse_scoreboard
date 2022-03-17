80ecee6853fd161da28621580c4282e702906b34
Christian Hall <ch0164@uah.edu>
2022-03-16 22:44:11 -0500

    Now redirecting the user to the home page as a logged-in user after user registration.

--- user_registration/views.py
+++ user_registration/views.py
@@ -1,8 +1,7 @@
-from http.client import HTTPResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from .forms import UserRegistrationForm
-from core.models import Roster
+from django.contrib.auth import authenticate, login
 
 
 # Create your views here.
@@ -10,14 +9,11 @@ def register(request: HttpRequest) -> HttpResponse:
     if request.method == "POST":
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
-            # if form.fields["is_coach"] is True:
-            #     name = form.cleaned_data["username"]
-            #     roster = Roster(name=name)
-            #     roster.save()
-            #     request.user.roster.add(roster)
-
             form.save()
-            return redirect("/")
+            new_user = authenticate(username=form.cleaned_data["username"],
+                                    password=form.cleaned_data["password1"])
+            login(request, new_user)
+            return HttpResponseRedirect("/")
     else:
         form = UserRegistrationForm()
         
